d70a54517431b3817249dcdced53c839a0b3ed81
Marcin Mirecki <marcin.mirecki@poleng.pl>
2023-04-24 14:09:10 +0200

    hword

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.css"
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
+
+  import "bootstrap/dist/js/bootstrap.js"
