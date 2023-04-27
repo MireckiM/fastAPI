9e8f62433d0cabf604f8d2c91e7516c96c8738b4
Marcin Mirecki <marcin.mirecki@poleng.pl>
2023-04-27 14:06:18 +0200

    req

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -10,20 +10,22 @@
               type="text"
               class="form-control col-3 mx-2"
               placeholder="Tytuł"
+              v-model="book.title"
             />
           </div>
-          <div class="col">
+          <!--<div class="col">
             <input
               type="text"
               class="form-control col-3 mx-2"
               placeholder="Autor"
             />
-          </div>
+          </div>-->
           <div class="col">
             <input
               type="text"
               class="form-control col-3 mx-2"
               placeholder="Strony"
+              v-model="book.pages"
             />
           </div>
           <div class="col">
@@ -31,6 +33,7 @@
               type="number"
               class="form-control col-3 mx-2"
               placeholder="ID klienta"
+              v-model="book.client_id"
             />
           </div>
           <div class="col-2"></div>
@@ -41,6 +44,7 @@
             <button
               class="btn btn-success"
               style="margin-top: 20px; width: 100%"
+              @click="addBook(book)"
             >
               Dodaj
             </button>
@@ -53,14 +57,14 @@
     <table class="table">
       <thead>
         <th>Tytuł</th>
-        <th>Autor</th>
+        <!--<th>Autor</th>-->
         <th>Strony</th>
         <th>Klient</th>
       </thead>
       <tbody>
         <tr v-for="book in books" :key="book.id" @dblclick="$data.book = book">
           <td>{{ book.title }}</td>
-          <td>{{ book.author }}</td>
+          <!--<td>{{ book.author }}</td>-->
           <td>{{ book.pages }}</td>
           <td>{{ book.client_id }}</td>
           <td>
@@ -93,9 +97,24 @@ export default {
     submitForm() {},
     async getBooks() {
       var response = await fetch("http://0.0.0.0:8001/books/");
+      console.log(response);
       this.books = await response.json();
     },
-    async addBook() {},
+    async addBook() {
+      await this.getBooks();
+      var response = await fetch("http://0.0.0.0:8001/add-book/", {
+        method: "post",
+        headers: {
+          //title: this.book.title,
+          //pages: this.book.pages,
+          //client_id: this.book.client_id,
+          //"Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.book),
+      });
+      console.log(response);
+      await this.getBooks();
+    },
     async editBook() {},
     async deleteBook() {},
     clear() {
