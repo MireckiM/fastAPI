47063464ff79caf97843008017945d626c855205
Marcin Mirecki <marcin.mirecki@poleng.pl>
2023-05-18 11:29:39 +0200

    router_1

---
+++ frontend/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from '../components/HelloWorld.vue'
+import HelloBooks from '../components/HelloBooks.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HelloWorld
+  },
+  {
+    path: '/library',
+    name: 'Biblioteka',
+    component: HelloBooks
+  },
+  
+]
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+export default router
